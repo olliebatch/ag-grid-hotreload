daf7c57a774c28717f15109f5f212fb11810fded
Ollie Batchelor <ollie.batchelor@red-badger.com>
2025-06-03 15:39:40 +0100

    basic repo

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import { AllCommunityModule, ModuleRegistry, type ColDef } from 'ag-grid-community'; 
+import { AgGridReact, type CustomCellRendererProps } from 'ag-grid-react';
+
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+interface IRow {
+  testRender: string;
+  failedCustomRenderer: { name: string; size: string };
+}
+
+
+const customRender = (params: CustomCellRendererProps) => {
+  return (
+    <div>Hello World</div>
+  );
+};
+
+function App() {
+  const [rowData, setRowData] = useState<IRow[]>([
+    {
+      testRender: 'Test',
+      failedCustomRenderer: { name: 'Test1234', size: '17071' },
+    },
+  ]);
+
+  // Column Definitions: Defines & controls grid columns.
+  const [colDefs, setColDefs] = useState<ColDef<IRow>[]>([
+    {
+      field: 'failedCustomRenderer',
+      headerName: 'Test Failed Render',
+      pinned: 'left',
+      lockPinned: true,
+      cellRenderer: customRender,
+      autoHeight: true,
+    },
+    { field: 'testRender', headerName: 'Test Render' }
+  ]);
+
+  return (
+    <div className='testRender'>
+    <AgGridReact
+        rowData={rowData}
+        columnDefs={colDefs}
+      />
+    </div>
+  )
+}
+
+export default App
